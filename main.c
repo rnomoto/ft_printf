c39ae5c185f669541b15806cc92ae317f8b8fc98
rnomoto <r.nomoto12345@gmail.com>
2024-10-13 17:51:48 +0900

    should think aout return

--- main.c
+++ main.c
@@ -6,60 +6,70 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:34:41 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/10/11 21:01:46 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/10/13 17:40:24 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "ft_printf.h"
 
-int ft_printf(const char *format, ...)
+void	converse(const char *format, va_list ap)
 {
-    va_list ap;
-    int d;
-    char c;
-    char *s;
-    int conv;
+	int	conv;
 
-    va_start(ap, format);
-    while(*format)
-    {
-        if(*format != '%')
-            ft_putchar(*format);
-        else if(*format == '%')
-        {
-            format++;
-            conv = *format;
-            if(conv == 'c')
-            {
-                c = va_arg(ap, int);
-                //why int?
-                ft_putchar(c);
-            }
-            else if(conv == 's')
-            {
-                s = va_arg(ap, char *);
-                ft_putstr(s);
-            }
-            else if(conv == 'd' || conv == 'i')
-            {
-                d = va_arg(ap, int);
-                ft_putnbr(d);
-            }
-        }
-        format++;
-    }
-    va_end(ap);
-    return (ft_strlen(format));
+	conv = *format;
+	if (conv == 'c')
+		case_c(ap);
+	else if (conv == 's')
+		case_s(ap);
+	else if (conv == 'p')
+		case_p(ap);
+	else if (conv == 'd' || conv == 'i')
+		case_d(ap);
+	else if (conv == 'u')
+		case_u(ap);
+	else if (conv == 'x')
+		case_lx(ap);
+	else if (conv == 'X')
+		case_ux(ap);
+	else if (conv == '%')
+		ft_putchar('%');
+	format++;
 }
 
-int main(void)
+int	ft_printf(const char *format, ...)
 {
-    int d = 42;
-    char c = 'A';
-    char str[] = "hello";
-    ft_printf("hello %c hello\n", c);
-    ft_printf("hello %d hello\n", d);
-    ft_printf("hello %s hello\n", str);
+	va_list	ap;
 
-    return 0;
+	va_start(ap, format);
+	while (*format)
+	{
+		if (*format != '%')
+			ft_putchar(*format);
+		else if (*format == '%')
+		{
+			format++;
+			converse(format, ap);
+		}
+		format++;
+	}
+	va_end(ap);
+	return (ft_strlen(format));
 }
+
+// int main(void)
+// {
+//     int d = -42;
+//     char c = 'A';
+//     char str[] = "hello";
+//     unsigned int u = 42;
+//     ft_printf("hello %c hello%c\n", c, c);
+//     ft_printf("hello %d hello\n", d);
+//     ft_printf("hello %s hello\n", str);
+//     ft_printf("%p\n", str);
+//     ft_printf("hello %u hello\n", u);
+//     ft_printf("hello %x hello\n", d);
+//     ft_printf("hello %X hello\n", d);
+//     ft_printf("hello %% hello\n");
+
+//     return (0);
+// }
