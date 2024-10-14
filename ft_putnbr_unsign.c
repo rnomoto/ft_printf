ba9d73c69957b7e95b22923c59bbe3a30411797b
rnomoto <r.nomoto12345@gmail.com>
2024-10-14 17:14:15 +0900

    made int return

--- ft_putnbr_unsign.c
+++ ft_putnbr_unsign.c
@@ -6,17 +6,17 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 14:40:36 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/10/13 17:43:11 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/10/14 15:18:04 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putnbr_unsign(unsigned int nb)
+int	ft_putnbr_unsign(unsigned int nb, int count)
 {
 	if (nb > 9)
-	{
-		ft_putnbr_unsign(nb / 10);
-	}
+		count = ft_putnbr_unsign(nb / 10, count);
 	ft_putchar(nb % 10 + 48);
+	count += 1;
+	return count;
 }
