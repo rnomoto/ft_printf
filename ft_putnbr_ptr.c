e006264b612cad87cdb07752c47c6a1eb0339dc1
rnomoto <r.nomoto12345@gmail.com>
2024-10-14 17:13:54 +0900

    made int return

--- ft_putnbr_ptr.c
+++ ft_putnbr_ptr.c
@@ -6,19 +6,31 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 13:52:48 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/10/13 14:47:56 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/10/14 15:55:15 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putnbr_ptr(uintptr_t nbr)
+int	ft_putnbr_ptr(uintptr_t nbr, int count)
 {
 	char *base = "0123456789abcdef";
 
-	if (nbr >= 16)
-	{
-		ft_putnbr_ptr(nbr / 16);
-	}
+	if (nbr > 15)
+		count = ft_putnbr_ptr(nbr / 16, count);
 	ft_putchar(base[nbr % 16]);
+	count += 1;
+	return count;
 }
+
+// int main(void)
+// {
+// 	char str[] = "Hello World!";
+// 	char **str_ptr = &str;
+// 	uintptr_t nbr = (uintptr_t)str_ptr;
+// 	int count = 0;
+// 	count = ft_putnbr_ptr(nbr, count);
+// 	printf("\n%d\n", count);
+
+// 	return 0;
+// }
