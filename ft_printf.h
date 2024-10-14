bdc4ca83a2ca74761193fa82406e2a111b472370
rnomoto <r.nomoto12345@gmail.com>
2024-10-14 17:14:43 +0900

    prototype changed

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:41:45 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/10/13 17:36:03 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/10/14 15:30:49 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,19 @@
 # include <stdint.h>
 
 int		ft_printf(const char *, ...);
-void	case_c(va_list ap);
-void	case_d(va_list ap);
-void	case_lx(va_list ap);
-void	case_p(va_list ap);
-void	case_s(va_list ap);
-void	case_u(va_list ap);
-void	case_ux(va_list ap);
-void	ft_putaddress(void *s);
+int	case_c(va_list ap, int count);
+int	case_d(va_list ap, int count);
+int	case_lx(va_list ap, int count);
+int	case_p(va_list ap, int count);
+int	case_s(va_list ap, int count);
+int	case_u(va_list ap, int count);
+int	case_ux(va_list ap, int count);
+int	ft_putaddr(void *s, int count);
 void	ft_putchar(char c);
-void	ft_putnbr_case(int nbr, char case_type);
-void	ft_putnbr_ptr(uintptr_t nbr);
-void	ft_putnbr_unsign(unsigned int nb);
-void	ft_putnbr(int nb);
+int	ft_putnbr_case(int nbr, int count, char case_type);
+int	ft_putnbr_count(int nb, int count);
+int	ft_putnbr_ptr(uintptr_t nbr, int count);
+int	ft_putnbr_unsign(unsigned int nb, int count);
 void	ft_putstr(char *str);
-void	ft_putnbr_ptr(uintptr_t nbr);
 
 #endif
