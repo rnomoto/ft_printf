3d5022e9e9588188bce4f0c53f8e7922937307d2
rnomoto <r.nomoto12345@gmail.com>
2024-10-14 17:12:18 +0900

    name changed and made int return

--- ft_putaddr.c
+++ ft_putaddr.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putaddress.c                                    :+:      :+:    :+:   */
+/*   ft_putaddr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 12:57:31 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/10/13 14:48:10 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/10/14 15:58:06 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putaddress(void *s)
+int	ft_putaddr(void *s, int count)
 {
 	void		**ad;
 	uintptr_t	adr;
@@ -20,13 +20,17 @@ void	ft_putaddress(void *s)
 	ad = &s;
 	adr = (uintptr_t)ad;
 	write(1, "0x", 2);
-	ft_putnbr_ptr(adr);
+    count += 2;
+	count += ft_putnbr_ptr(adr, count - 2);
+    return count;
 }
 
 // int main(void)
 // {
 //     char *str = "Hello World!";
-//     ft_putaddress(str);
+// 	int count = 0;
+//     int n = ft_putaddr(str, count);
+// 	printf("\n%d\n", n);
 
 //     return (0);
 // }
