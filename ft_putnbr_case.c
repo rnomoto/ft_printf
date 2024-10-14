852ed2449db5fa14480e247e275a8afd76c259e9
rnomoto <r.nomoto12345@gmail.com>
2024-10-14 17:13:33 +0900

    made int return

--- ft_putnbr_case.c
+++ ft_putnbr_case.c
@@ -6,13 +6,13 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 14:51:03 by rnomoto           #+#    #+#             */
-/*   Updated: 2024/10/13 15:12:17 by rnomoto          ###   ########.fr       */
+/*   Updated: 2024/10/14 15:53:07 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putnbr_case(int nbr, char case_type)
+int	ft_putnbr_case(int nbr, int count, char case_type)
 {
 	char *base = NULL;
 	if (case_type == 'a')
@@ -23,10 +23,13 @@ void	ft_putnbr_case(int nbr, char case_type)
 	{
 		nbr *= -1;
 		ft_putchar('-');
+		count += 1;
 	}
 	if (nbr >= 16)
 	{
-		ft_putnbr_case(nbr / 16, case_type);
+		count = ft_putnbr_case(nbr / 16, count, case_type);
 	}
 	ft_putchar(base[nbr % 16]);
+	count += 1;
+	return count;
 }
